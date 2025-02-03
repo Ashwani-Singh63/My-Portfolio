d96c069cf4bf3040369bf1bf5dd0ec9b4744d281
Ashwani-Singh63 <ashwanisingh638796@gmail.com>
2025-02-03 12:25:01 +0530

    Project setup
    setting the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      {/* <div className='text-blue-500 bg-red-600'>Hello </div> */}
+      <Home/>
+    </>
+  )
+}
+
+export default App
